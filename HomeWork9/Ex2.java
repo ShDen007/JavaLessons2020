970115c3603788c8205765ee5c8d45d008655732
ShDen007 <shden@meta.ua>
2020-08-30 16:15:50 +0300

    9

--- HomeWork9/Ex2.java
+++ HomeWork9/Ex2.java
@@ -1,5 +1,6 @@
 package HomeWork9;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 //Задание2.Создать квадратный массив размерностью NxN заполненный случайными числами,
@@ -17,35 +18,75 @@ public class Ex2 {
         System.out.println("Input number columns");
         int column = scanner.nextInt();
 
-
-
         int[][] arrMulti = new int[row][column];
         int iter = 0;
+        int min = 0;
+        int max = 0;
+        int i = 0;
+        int j = 0;
 
-        for (int i = 0; i < row; i++) {
-            for (int j = 0; j < column; j++) {
+        for (i = 0; i < row; i++) {
+            for (j = 0; j < column; j++) {
                 arrMulti[i][j] = (int) Math.round((Math.random() * 50) + 0);
             }
         }
+        for (int[] anArrMulti : arrMulti) {
+            for (int anAnArrMulti : anArrMulti) {
+            }
+            System.out.print(Arrays.toString(anArrMulti) + " ");
+            System.out.println();
+        }
+        double startTime = System.nanoTime();
 
+        for(i = 0; i < row; i++){
+            for(j = 0; j < column; j++){
+                if (min > arrMulti[i][j]){
+                    min = arrMulti[i][j];
+                }
+                if( max < arrMulti[i][j]){
+                    max = arrMulti[i][j];
+                }
+            }
+        }
+        System.out.println("Min : " + min);
+        System.out.println("Max : "+ max);
+        for(i = 0; i < row; i++){
+            for(j = 0; j < column; j++){
+                if(min == arrMulti[i][j]){
+                    arrMulti[0][iter] = i;
+                    arrMulti[1][iter] = j;
+                    iter++;
+                }
+            }
+        }
+        if(iter > 1){
+            System.out.println("coincidence at min");
+            for(i = 0; i < iter; i++){
+                System.out.print(arrMulti[0][i]+ ", " + arrMulti[1][i]+ ", ");
+            }
+        }
+        for(i = 0; i < row; i++){
+            for(j = 0; j < column; j++){
+                if(max == arrMulti[i][j]){
+                    arrMulti[0][iter] = i;
+                    arrMulti[1][iter] = j;
+                    iter++;
+                }
+            }
+        }
+        if(iter > 1) {
+            System.out.println("coincidence at max");
+            for (i = 0; i < iter; i++) {
+                System.out.print(arrMulti[0][i] + ", " + arrMulti[1][i] + ", ");
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+            }
+        }
+        System.out.println();
+        double stop = System.nanoTime();
+        double time = stop - startTime;
+        System.out.println("Time searching : "+time);
 
 scanner.close();
     }
+
 }
--- HomeWork9/Ex3.java
+++ HomeWork9/Ex3.java
@@ -1,5 +1,7 @@
 package HomeWork9;
 
+import java.util.Scanner;
+
 //Задние3.Заполните n мерный квадратный массив числами, которые увеличиваются на 1 по
 // * спирали (число n должно задаваться с клавиатуры). Выведите результат на экран
 // * с соблюдением ширины столбцов. Выполнить задачу с использованием только одного цикла.
@@ -13,10 +15,89 @@ package HomeWork9;
 public class Ex3 {
     public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter number n (row array)");
+        int n = scanner.nextInt();
+        System.out.println("Enter number nm (column array)");
+        int m = scanner.nextInt();
+        int iter = 1;
+        int a = 1;
+        int b = 1;
 
+        int[][] array = new int[m][n];
 
+            for (int i = 0; i < n; i++) {
+                array[0][i] = iter;
+                iter++;
+            }
+            for (int j = 1; j < m; j++) {
+                array[j][n - 1] = iter;
+                iter++;
+            }
+            for (int i = n - 2; i >= 0; i--) {
+                array[m - 1][i] = iter;
+                iter++;
+            }
+            for (int j = m - 2; j > 0; j--) {
+                array[j][0] = iter;
+                iter++;
+            }
+            while (iter < m * n) {
 
+                while (array[a][b + 1] == 0) {
+                    array[a][b] = iter;
+                    iter++;
+                    b++;
+                }
 
+                while (array[a + 1][b] == 0) {
+                    array[a][b] = iter;
+                    iter++;
+                    a++;
+                }
 
+                while (array[a][b - 1] == 0) {
+                    array[a][b] = iter;
+                    iter++;
+                    b--;
+                }
+
+                while (array[a - 1][b] == 0) {
+                    array[a][b] = iter;
+                    iter++;
+                    a--;
+                }
+            }
+
+            for (int j = 0; j < m; j++) {
+                for (int i = 0; i < n; i++) {
+                    if (array[j][i] == 0) {
+                        array[j][i] = iter;
+                    }
+                }
+            }
+
+            for (int j = 0; j < m; j++) {
+                for (int i = 0; i < n; i++) {
+                    if (array[j][i] < 10) {
+                        System.out.print(array[j][i] + ",   ");
+                    } else {
+                        System.out.print(array[j][i] + ",  ");
+                    }
+                }
+                System.out.println("");
+            }
+    scanner.close();
     }
 }
+
+
+
+
+
+
+
+
+
+
+
