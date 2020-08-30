6098677c7c73c86b317bb2ab37f6b2a446ad3604
ShDen007 <shden@meta.ua>
2020-08-30 14:54:08 +0300

    9

--- HomeWork9/Ex1.java
+++ HomeWork9/Ex1.java
@@ -2,31 +2,48 @@ package HomeWork9;
 
 import java.util.Arrays;
 import java.util.Random;
+import java.util.Scanner;
 
 //Здание1. Заполните двухмерный массив размерностью NxM случайными числами и построчно перенесите эти
 //числа в одномерный массив. Вывести исходный массив и полученный масcивы.
 public class Ex1 {
     public static void main(String[] args) {
 
-        int[][] arr = new int[4][4];
-        for (int i = 0; i < arr.length; i++) {
-            for (int j = 0; j < arr[i].length; j++) {
-                arr[i][j] = (int) Math.round((Math.random() * 50) + 0);
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Input number rows");
+        int row = scanner.nextInt();
+        System.out.println("Input number columns");
+        int column = scanner.nextInt();
+        System.out.println("Multi Array");
+
+
+        int[][] arrMulti = new int[row][column];
+        int iter = 0;
+
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < column; j++) {
+                arrMulti[i][j] = (int) Math.round((Math.random() * 50) + 0);
             }
         }
-        for (int[] anArr : arr) {
-            for (int anAnArr : anArr) {
-         int []arr1 = new int[16];
+        for (int[] anArrMulti : arrMulti) {
+            for (int anAnArrMulti : anArrMulti) {
 
-
-
-                System.out.print(anAnArr + " ");
-            }System.out.print(Arrays.toString(anArr) + " ");
+            }System.out.print(Arrays.toString(anArrMulti) + " ");
             System.out.println();
         }
+         int []arr = new int[row*column];
 
-
-
+         for(int i = 0; i < row; i++){
+             for(int j = 0; j < column;j++ ){
+               arr[iter] = arrMulti[i][j];
+               iter++;
+             }
+         }
+        System.out.println("one - dimensional array");
+         for(int c : arr){
+             System.out.print(
+                     c + " ");
+        }
+         scanner.close();
     }
-
 }
